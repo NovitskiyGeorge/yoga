7d155727598a7d3d10f76214fbee6cf0ae1ded3d
George <novitskiy.gs@gmail.com>
2020-10-17 10:28:37 +0300

    lesson 3.8

--- js/scripttest.js
+++ js/scripttest.js
@@ -1,6 +1,12 @@
 
 let age = document.getElementById('age');
+
 function showUser(surname, name) {
 alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
 }
-showUser('Дятел', 'полный');
+
+age.addEventListener('change', function(){
+    showUser.apply(age, ['Dimitriy', 'Komarov']);       
+});
+
+
